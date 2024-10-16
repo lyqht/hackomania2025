b05ad57737690f0f3ea8aa54127a51b1cccfaf1b
lyqht <estee.tsw@gmail.com>
2024-10-17 00:35:50 +0800

    add next.config.js for locales

--- next.config.js
+++ next.config.js
@@ -1,4 +1,42 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	i18n: {
+		defaultLocale: "en",
+		// Below are locales supported by DeepL
+		locales: [
+			"bg",
+			"cs",
+			"da",
+			"de",
+			"el",
+			"en",
+			"es",
+			"et",
+			"fi",
+			"fr",
+			"hu",
+			"id",
+			"it",
+			"ja",
+			"ko",
+			"lt",
+			"lv",
+			"nb",
+			"nl",
+			"pl",
+			"pt-BR",
+			"pt-PT",
+			"ro",
+			"ru",
+			"sk",
+			"sl",
+			"sv",
+			"tr",
+			"uk",
+			"zh-HANS",
+			"zh",
+		],
+	},
+};
 
 module.exports = nextConfig;
