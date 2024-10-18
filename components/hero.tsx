0fb007cab2565e3250ad991b51dddf4c9e3956cc
lyqht <estee.tsw@gmail.com>
2024-10-18 22:45:14 +0800

    update Hero h1 text and img tag to next/image

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,16 +1,23 @@
+import Image from "next/image";
 export default function Hero() {
   return (
     <div className="flex flex-col items-center gap-16">
       <div className="flex items-center justify-center gap-8">
         <a href="https://geekshacking.com">
-          <img width={180} src="/geekshacking.webp" alt="GeeksHacking Singapore" />
+          <Image
+            width={180}
+            height={120}
+            layout="responsive"
+            src="/geekshacking.webp"
+            alt="GeeksHacking Singapore"
+          />
         </a>
       </div>
-      <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
+      <h1 className="sr-only">HackOMania 2025</h1>
       <p className="mx-auto max-w-xl text-center text-3xl !leading-tight lg:text-4xl">
         Get <b>Healthy</b>, Get <b>Connected</b>
       </p>
-      <div className="my-8 w-full bg-gradient-to-r from-transparent via-foreground/10 to-transparent p-[1px]" />
+      <div className="mt-8 w-full bg-gradient-to-r from-transparent via-foreground/10 to-transparent p-[1px]" />
     </div>
   );
 }
