dcf3f1984bb94bd16b64f258366c883d9c8bac05
lyqht <estee.tsw@gmail.com>
2024-10-20 13:03:17 +0800

    remove unused file and imports for lint

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -4,15 +4,15 @@ import { GithubIcon } from "lucide-react";
 
 export default function Login() {
   return (
-    <div className="flex flex-col min-w-64 max-w-64 mx-auto">
+    <div className="mx-auto flex min-w-64 max-w-64 flex-col">
       <h1 className="text-2xl font-medium">Sign in</h1>
       <p className="text-sm text-foreground">
-        Don't have an account?{" "}
-        <Link className="text-primary font-medium underline" href="/sign-up">
+        Don&apos;t have an account?{" "}
+        <Link className="font-medium text-primary underline" href="/sign-up">
           Sign up
         </Link>
       </p>
-      <div className="flex flex-col gap-2 mt-8">
+      <div className="mt-8 flex flex-col gap-2">
         <Button asChild className="w-full">
           <Link href="/auth/github">
             <GithubIcon className="mr-2 h-4 w-4" />
