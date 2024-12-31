447a587bdfa9c16e7832487d2375e769b4c949c5
lyqht <estee.tsw@gmail.com>
2024-12-31 11:33:03 +0800

    chore(layout.tsx): remove commented out code and unused footer content for cleaner codebase and improved maintainability

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,28 +27,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <main className="flex min-h-screen flex-col items-center">
             <div className="flex w-full flex-1 flex-col items-center">
               <NavigtionBar />
-              {/* <nav className="flex h-16 w-full justify-center border-b border-b-foreground/10">
-                <div className="flex w-full max-w-5xl items-center justify-between p-3 px-5 text-sm">
-                  <div className="flex items-center gap-5 font-semibold">
-                    <Link href={"/"}>HackOMania 2025</Link>
-                  </div>
-                  {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
-                </div>
-              </nav> */}
               <div className="flex w-full flex-col">{children}</div>
-
               <footer className="mx-auto flex w-full items-center justify-center gap-8 border-t py-16 text-center text-xs">
-                <p>
-                  Powered by{" "}
-                  <a
-                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-                    target="_blank"
-                    className="font-bold hover:underline"
-                    rel="noreferrer"
-                  >
-                    Supabase
-                  </a>
-                </p>
                 <ThemeSwitcher />
               </footer>
             </div>
