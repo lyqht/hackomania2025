b5ff546e2503b09ed2e9836750681a23a77052f1
Ethan-Chew <ethanchewmh@gmail.com>
2025-01-02 22:45:46 +0800

    feat(api): Routes for Team CRUD

---
+++ app/api/user/exists/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import { db } from "@/utils/db";
+import { user } from "@/utils/db/schema/user";
+import { eq } from "drizzle-orm";
+
+// Note: This route takes in the user's GitHub Userna,e and check whether the user exists in the database
+export default async function GET(request: Request) {
+  const requestBody = await request.json();
+  const { ghUsername } = requestBody;
+
+  if (!ghUsername) {
+    return NextResponse.json({ error: "ghUsername is required" }, { status: 400 });
+  }
+
+  // Check if the user exists in the database
+  const userExists = await db.select().from(user).where(eq(user.githubUsername, ghUsername));
+
+  if (userExists.length === 0) {
+    return NextResponse.json({ status: 404 });
+  }
+
+  return NextResponse.json({ status: 200 });
+}
