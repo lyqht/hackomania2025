2d9a5f4d8fb1e4d50fc3ffd809736106e08f0616
Ethan-Chew <ethanchewmh@gmail.com>
2025-01-05 17:18:29 +0800

    feat(user-portal): Main UI for User Portal

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -1,10 +1,6 @@
-import UserNavigationBar from "@/components/custom/(user-side)/UserNavigationBar";
-
 export default function AuthenticatedLayout({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      <UserNavigationBar />
-
       {children}
     </div>
   );
--- components/custom/(user-side)/UserNavigationBar.tsx
+++
@@ -1,3 +0,0 @@
-export default function UserNavigationBar() {
-  return <div></div>;
-}
