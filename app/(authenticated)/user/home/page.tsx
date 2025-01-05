38b1d0123cd610cdb2a46181c5a2fb4977c6e6d6
Ethan-Chew <ethanchewmh@gmail.com>
2025-01-05 17:18:29 +0800

    feat(user-portal): Main UI for User Portal

--- app/(authenticated)/user/home/page.tsx
+++ app/(authenticated)/user/home/page.tsx
@@ -1,3 +1,4 @@
+import NoTeamManagement from "@/components/custom/NoTeamManagement";
 import { getUser } from "@/utils/supabase/user";
 import { User } from "@supabase/supabase-js";
 import { headers } from "next/headers";
@@ -23,13 +24,43 @@ export default async function UserHome() {
   return (
     <div className="flex flex-col gap-5 p-5 md:p-20">
       <div>
-        <h1 className="mb-1 text-4xl font-bold">HackOMania 2025 User Portal</h1>
-        <p className="text-2xl text-neutral-600">Hello 👋, {user.githubUsername}</p>
+        <h1 className="mb-1 text-2xl font-bold md:text-4xl">HackOMania 2025 User Portal</h1>
+        <p className="text-xl text-neutral-600 md:text-2xl">Hello 👋, {user.githubUsername}</p>
       </div>
 
-      <div className="rounded-lg border border-neutral-400 p-5">
-        <section>
-          <h2 className="text-xl font-semibold">Your Team</h2>
+      <div className="rounded-lg border border-neutral-400">
+        <section className="p-5">
+          <h2 className="mb-3 text-2xl font-semibold">Your Information</h2>
+          <div className="flex flex-col justify-around gap-2 md:flex-row md:text-center">
+            <div>
+              <p className="text-xl font-medium">Applicant ID</p>
+              <p>{user.id}</p>
+            </div>
+            <div>
+              <p className="text-xl font-medium">Registration Status</p>
+              <p>Not Registered</p>
+            </div>
+            <div>
+              <p className="text-xl font-medium">GitHub Account</p>
+              <p>{user.githubUsername}</p>
+            </div>
+          </div>
+        </section>
+
+        <div className="my-3 border border-neutral-400"></div>
+
+        <section className="p-5">
+          <h2 className="text-2xl font-semibold">Your Team</h2>
+          <div id="no-team" className="flex flex-col justify-center">
+            <p>
+              You have not joined a team. You may either create your own team, or join an existing
+              team using it&apos;s Team ID.
+            </p>
+            <div className="mt-2">
+              <NoTeamManagement />
+            </div>
+          </div>
+          <div id="team-details"></div>
         </section>
       </div>
     </div>
---
+++ components/custom/NavigationBar/NavigationBar.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import styles from "./NavigationBar.module.css";
+import { useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { FaXmark } from "react-icons/fa6";
+
+const navigationLinks = [
+  { title: "REGISTRATION", href: "#registration" },
+  { title: "ABOUT", href: "#about" },
+  { title: "CHALLENGES", href: "#challenges" },
+  { title: "PRIZES", href: "#prizes" },
+  { title: "TIMELINE", href: "#timeline" },
+  { title: "VENUE", href: "#venue" },
+  { title: "SPONSORS", href: "#sponsors" },
+  { title: "TEAM", href: "#team" },
+  { title: "CONTACT", href: "#contact" },
+];
+
+export default function NavigationBar() {
+  const [expandMobileNav, setExpandMobileNav] = useState(false);
+
+  // TODO: Add Framer Motion Animate
+
+  return (
+    <div className={`w-screen ${styles.grid} pb-10`}>
+      <div className="m-2 bg-hackomania-red p-5 md:mx-20 md:my-10">
+        <nav className="hidden flex-row justify-end gap-3 text-white md:flex" id="full-nav">
+          {navigationLinks.map((link) => (
+            <a key={link.title} href={link.href} className="text-white">
+              {link.title}
+            </a>
+          ))}
+        </nav>
+
+        {expandMobileNav ? (
+          <FaXmark
+            className="ml-auto block text-2xl text-white md:hidden"
+            onClick={() => setExpandMobileNav(false)}
+          />
+        ) : (
+          <RxHamburgerMenu
+            className="ml-auto block text-2xl text-white md:hidden"
+            onClick={() => setExpandMobileNav(true)}
+          />
+        )}
+
+        {expandMobileNav && (
+          <nav className="flex flex-col gap-3" id="mobile-nav">
+            {navigationLinks.map((link) => (
+              <a key={link.title} href={link.href} className="text-white">
+                {link.title}
+              </a>
+            ))}
+          </nav>
+        )}
+      </div>
+    </div>
+  );
+}
