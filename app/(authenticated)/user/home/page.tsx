2c53f7f10a9b3aafc96d888776cebdc820097c77
Ethan Chew <ethanchewmh@gmail.com>
2025-01-05 23:41:18 +0800

    feat(manage team): create, delete team and delete members

--- app/(authenticated)/user/home/page.tsx
+++ app/(authenticated)/user/home/page.tsx
@@ -1,7 +1,10 @@
-import NoTeamManagement from "@/components/custom/NoTeamManagement";
+import EditTeamButtons from "@/components/custom/user-team/EditTeamButtons";
+import NoTeamManagement from "@/components/custom/user-team/NoTeamManagement";
+import TeamManagement from "@/components/custom/user-team/TeamManagement";
 import { getUser } from "@/utils/supabase/user";
 import { User } from "@supabase/supabase-js";
 import { headers } from "next/headers";
+import Link from "next/link";
 
 export default async function UserHome() {
   const host = headers().get("host");
@@ -20,12 +23,24 @@ export default async function UserHome() {
   }
 
   const { user } = await retrieveUserResponse.json();
+  let userTeam;
+  if (user.teamName) {
+    const retrieveTeamResponse = await fetch(`http://${host!}/api/team?id=${user.teamId}`, {
+      method: "GET",
+    });
+
+    if (retrieveTeamResponse.ok) {
+      userTeam = await retrieveTeamResponse.json();
+    }
+  }
 
   return (
     <div className="flex flex-col gap-5 p-5 md:p-20">
       <div>
         <h1 className="mb-1 text-2xl font-bold md:text-4xl">HackOMania 2025 User Portal</h1>
-        <p className="text-xl text-neutral-600 md:text-2xl">Hello 👋, {user.githubUsername}</p>
+        <p className="text-xl text-neutral-600 md:text-2xl">
+          <span className="font-medium">Hello 👋,</span> {user.githubUsername}
+        </p>
       </div>
 
       <div className="rounded-lg border border-neutral-400">
@@ -42,7 +57,7 @@ export default async function UserHome() {
             </div>
             <div>
               <p className="text-xl font-medium">GitHub Account</p>
-              <p>{user.githubUsername}</p>
+              <Link href={`https://github.com/${user.githubUsername}`}>{user.githubUsername}</Link>
             </div>
           </div>
         </section>
@@ -50,8 +65,15 @@ export default async function UserHome() {
         <div className="my-3 border border-neutral-400"></div>
 
         <section className="p-5">
-          <h2 className="text-2xl font-semibold">Your Team</h2>
-          <div id="no-team" className="flex flex-col justify-center">
+          <div className="mb-3 flex flex-col gap-3 md:flex-row md:items-center">
+            <h2 className="text-2xl font-semibold">
+              Your Team {user.teamName && <span className="font-normal">: {user.teamName}</span>}
+            </h2>
+            {user.teamName && userTeam.leaderId === user.id && (
+              <EditTeamButtons teamID={userTeam.id} />
+            )}
+          </div>
+          <div id="no-team" className={`flex flex-col justify-center ${user.teamName && "hidden"}`}>
             <p>
               You have not joined a team. You may either create your own team, or join an existing
               team using it&apos;s Team ID.
@@ -60,7 +82,20 @@ export default async function UserHome() {
               <NoTeamManagement />
             </div>
           </div>
-          <div id="team-details"></div>
+
+          <div
+            id="team-details"
+            className={`flex flex-col justify-center ${!user.teamName && "hidden"}`}
+          >
+            <p className="text-neutral-500">
+              <span className="font-medium">Number of Members: </span>
+              {userTeam.users.length}/5
+            </p>
+
+            <div className="mt-2">
+              <TeamManagement users={userTeam.users} teamId={userTeam.id} />
+            </div>
+          </div>
         </section>
       </div>
     </div>
