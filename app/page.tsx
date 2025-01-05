33a270bcba31d933ff9b0d80f63362e30720f3b2
lyqht <estee.tsw@gmail.com>
2025-01-05 17:59:38 +0800

    fix dom structure

--- app/page.tsx
+++ app/page.tsx
@@ -13,12 +13,9 @@ export default async function Index() {
         <head>{/* HackOMania 2025 Logo goes here */}</head>
 
         <section className="py-10" id="about">
-          <div
-            className="mb-7 flex flex-row items-center gap-3 text-3xl font-bold text-hackomania-red md:mb-14 md:text-6xl"
-            id="about-title"
-          >
+          <div className="mb-7 flex flex-row items-center gap-3 text-3xl font-bold text-hackomania-red md:mb-14 md:text-6xl">
             <Image src="/RedStar.svg" alt="" width={60} height={60} />
-            <h1>HACKOMANIA 2024</h1>
+            <h2>HACKOMANIA 2024</h2>
           </div>
 
           <div className="flex flex-col gap-5 md:flex-row">
@@ -87,10 +84,7 @@ export default async function Index() {
         </section>
 
         <section className="py-10" id="prizes">
-          <div
-            className="mb-7 flex flex-row items-center gap-3 fill-hackomania-green text-3xl font-bold text-hackomania-green md:mb-14 md:text-6xl"
-            id="prizes-title"
-          >
+          <div className="mb-7 flex flex-row items-center gap-3 fill-hackomania-green text-3xl font-bold text-hackomania-green md:mb-14 md:text-6xl">
             <Image src="/GreenStar.svg" alt="" width={60} height={60} />
             <h2>PRIZES</h2>
           </div>
@@ -107,10 +101,7 @@ export default async function Index() {
         </section>
         <TimelineSection />
         <section className="py-10" id="prizes">
-          <div
-            className="mb-5 flex flex-row items-center gap-3 text-3xl font-bold text-hackomania-blue md:mb-10 md:text-6xl"
-            id="rules-title"
-          >
+          <div className="mb-5 flex flex-row items-center gap-3 text-3xl font-bold text-hackomania-blue md:mb-10 md:text-6xl">
             <Image
               src="/BlueIcon.svg"
               alt=""
@@ -131,84 +122,85 @@ export default async function Index() {
         </section>
 
         <section className="py-10" id="venue">
-          <div
-            className="mb-5 flex flex-row items-center gap-3 text-3xl font-bold text-hackomania-green md:mb-10 md:text-6xl"
-            id="roles-title"
-          >
+          <div className="mb-5 flex flex-row items-center gap-3 text-3xl font-bold text-hackomania-green md:mb-10 md:text-6xl">
             <Image src="/GreenStar.svg" alt="" width={60} height={60} />
             <h2>VENUE</h2>
           </div>
 
-          <section className="py-10" id="pre-event">
-            <h2 className="mb-4 text-2xl font-bold text-hackomania-green md:text-5xl">PRE EVENT</h2>
+          <div>
+            <div className="py-10" id="pre-event">
+              <h3 className="mb-4 text-2xl font-bold text-hackomania-green md:text-5xl">
+                PRE EVENT
+              </h3>
 
-            <div className="flex flex-col gap-5 md:flex-row">
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.7946893513426!2d103.84796911744382!3d1.2979024999999969!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31da19ec2599519d%3A0x809fd655663da6d0!2sLazada%20One!5e0!3m2!1sen!2stw!4v1735830756269!5m2!1sen!2stw"
-                className="h-auto w-full border-0 md:w-1/2"
-                loading="lazy"
-              ></iframe>
+              <div className="flex flex-col gap-5 md:flex-row">
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.7946893513426!2d103.84796911744382!3d1.2979024999999969!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31da19ec2599519d%3A0x809fd655663da6d0!2sLazada%20One!5e0!3m2!1sen!2stw!4v1735830756269!5m2!1sen!2stw"
+                  className="h-auto w-full border-0 md:w-1/2"
+                  loading="lazy"
+                ></iframe>
 
-              <div>
-                <h3 className="mb-4 text-xl font-bold text-hackomania-green md:text-4xl">
-                  GETTING HERE
-                </h3>
-
-                <div className="mb-2">
-                  <h4 className="text-lg font-bold text-hackomania-green md:text-2xl">Address</h4>
-                  <p className="text-lg">
-                    51 Bras Basah Rd, #04-08 Lazada One
-                    <br />
-                    Singapore 189554
-                  </p>
-                </div>
-                <div className="mb-2">
-                  <h4 className="text-lg font-bold text-hackomania-green md:text-2xl">Date</h4>
-                  <p className="text-lg">8th February 2025</p>
-                </div>
                 <div>
-                  <h4 className="text-lg font-bold text-hackomania-green md:text-2xl">Time</h4>
-                  <p className="text-lg">9:30am to 4:30pm</p>
+                  <h4 className="mb-4 text-xl font-bold text-hackomania-green md:text-4xl">
+                    GETTING HERE
+                  </h4>
+
+                  <div className="mb-2">
+                    <p className="text-lg font-bold text-hackomania-green md:text-2xl">Address</p>
+                    <p className="text-lg">
+                      51 Bras Basah Rd, #04-08 Lazada One
+                      <br />
+                      Singapore 189554
+                    </p>
+                  </div>
+                  <div className="mb-2">
+                    <p className="text-lg font-bold text-hackomania-green md:text-2xl">Date</p>
+                    <p className="text-lg">8th February 2025</p>
+                  </div>
+                  <div>
+                    <p className="text-lg font-bold text-hackomania-green md:text-2xl">Time</p>
+                    <p className="text-lg">9:30am to 4:30pm</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </section>
 
-          <section className="py-10" id="main-event">
-            <h2 className="mb-4 text-2xl font-bold text-hackomania-green md:text-5xl">
-              MAIN EVENT
-            </h2>
-            <div className="flex flex-col gap-5 md:flex-row">
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.8203085885248!2d103.8481571806732!3d1.2815570723565077!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31da190dd3c9d033%3A0x4cadb1e9e5280c4a!2s138%20Market%20St%2C%20%2320-01%20CapitaGreen%2C%20Singapore%20048946!5e0!3m2!1sen!2stw!4v1735830681400!5m2!1sen!2stw"
-                className="h-auto w-full border-0 md:w-1/2"
-                loading="lazy"
-              ></iframe>
+            <div className="py-10" id="main-event">
+              <h3 className="mb-4 text-2xl font-bold text-hackomania-green md:text-5xl">
+                MAIN EVENT
+              </h3>
+              <div className="flex flex-col gap-5 md:flex-row">
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.8203085885248!2d103.8481571806732!3d1.2815570723565077!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31da190dd3c9d033%3A0x4cadb1e9e5280c4a!2s138%20Market%20St%2C%20%2320-01%20CapitaGreen%2C%20Singapore%20048946!5e0!3m2!1sen!2stw!4v1735830681400!5m2!1sen!2stw"
+                  className="h-auto w-full border-0 md:w-1/2"
+                  loading="lazy"
+                ></iframe>
 
-              <div>
-                <h3 className="mb-4 text-xl font-bold text-hackomania-green md:text-4xl">
-                  GETTING HERE
-                </h3>
-
-                <div className="mb-2">
-                  <h4 className="text-lg font-bold text-hackomania-green md:text-2xl">Address</h4>
-                  <p className="text-lg">
-                    138 Market St, #20-01 CapitaGreen
-                    <br />
-                    Singapore 048946
-                  </p>
-                </div>
-                <div className="mb-2">
-                  <h4 className="text-lg font-bold text-hackomania-green md:text-2xl">Date</h4>
-                  <p className="text-lg">15th to 16th February 2025</p>
-                </div>
                 <div>
-                  <h4 className="text-lg font-bold text-hackomania-green md:text-2xl">Time</h4>
-                  <p className="text-lg">15th February, 10:30am to 16th February, 5:30pm</p>
+                  <h4 className="mb-4 text-xl font-bold text-hackomania-green md:text-4xl">
+                    GETTING HERE
+                  </h4>
+
+                  <div className="mb-2">
+                    <p className="text-lg font-bold text-hackomania-green md:text-2xl">Address</p>
+                    <p className="text-lg">
+                      138 Market St, #20-01 CapitaGreen
+                      <br />
+                      Singapore 048946
+                    </p>
+                  </div>
+                  <div className="mb-2">
+                    <p className="text-lg font-bold text-hackomania-green md:text-2xl">Date</p>
+                    <p className="text-lg">15th to 16th February 2025</p>
+                  </div>
+                  <div>
+                    <p className="text-lg font-bold text-hackomania-green md:text-2xl">Time</p>
+                    <p className="text-lg">15th February, 10:30am to 16th February, 5:30pm</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </section>
+          </div>
         </section>
 
         <section className="py-8" id="sponsors">
@@ -221,10 +213,7 @@ export default async function Index() {
         </section>
 
         <section className="py-8" id="team">
-          <div
-            className="mb-7 flex flex-row items-center gap-3 text-3xl font-bold text-hackomania-red md:mb-14 md:text-6xl"
-            id="team-title"
-          >
+          <div className="mb-7 flex flex-row items-center gap-3 text-3xl font-bold text-hackomania-red md:mb-14 md:text-6xl">
             <Image
               src="/MeetTheTeam.svg"
               alt="Icon"
