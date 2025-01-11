543d6fd8fcad3963d8946d5e73b6ad51861e0ebb
lyqht <estee.tsw@gmail.com>
2025-01-11 11:23:12 +0800

    add framer motion animation to nav bar

--- components/custom/NavigationBar.tsx
+++ components/custom/NavigationBar.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { FaXmark } from "react-icons/fa6";
 import Link from "next/link";
+import { motion, AnimatePresence } from "framer-motion";
 
 const navigationLinks = [
   { title: "REGISTRATION", href: "#registration" },
@@ -27,9 +28,11 @@ export default function NavigtionBar() {
       <div className="m-2 bg-hackomania-red p-5 md:mx-20 md:my-10">
         <nav className="hidden flex-row justify-end gap-3 text-white md:flex" id="full-nav">
           {navigationLinks.map((link) => (
-            <Link key={link.title} href={link.href} className="text-white hover:underline">
-              {link.title}
-            </Link>
+            <motion.div key={link.title} whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}>
+              <Link href={link.href} className="text-white hover:underline">
+                {link.title}
+              </Link>
+            </motion.div>
           ))}
         </nav>
 
@@ -45,15 +48,30 @@ export default function NavigtionBar() {
           />
         )}
 
-        {expandMobileNav && (
-          <nav className="flex flex-col gap-3" id="mobile-nav">
-            {navigationLinks.map((link) => (
-              <Link key={link.title} href={link.href} className="text-white">
-                {link.title}
-              </Link>
-            ))}
-          </nav>
-        )}
+        <AnimatePresence>
+          {expandMobileNav && (
+            <motion.nav
+              className="flex flex-col gap-3"
+              initial={{ opacity: 0, height: 0 }}
+              animate={{ opacity: 1, height: "auto" }}
+              exit={{ opacity: 0, height: 0 }}
+              transition={{ duration: 0.2 }}
+            >
+              {navigationLinks.map((link) => (
+                <motion.div
+                  key={link.title}
+                  initial={{ x: -20, opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                  exit={{ x: -20, opacity: 0 }}
+                >
+                  <Link href={link.href} className="text-white">
+                    {link.title}
+                  </Link>
+                </motion.div>
+              ))}
+            </motion.nav>
+          )}
+        </AnimatePresence>
       </div>
       <div className={`grid-bg absolute h-96 w-full md:h-60`}></div>
     </div>
