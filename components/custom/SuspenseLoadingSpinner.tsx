4668a5d9938e42b986363a8227cc180cbaf88c5c
lyqht <estee.tsw@gmail.com>
2025-01-23 20:08:21 +0800

    fix(loading.tsx): rename LoadingSpinner component to SuspenseLoadingSpinner for consistency with other components
    fix(page.tsx): rename LoadingSpinner component to SuspenseLoadingSpinner for consistency with other components
    
    feat(ButtonLoadingSpinner.tsx): add ButtonLoadingSpinner component to display a loading spinner inside a button
    
    feat(SuspenseLoadingSpinner.tsx): add SuspenseLoadingSpinner component to display a loading spinner with suspense fallback
    
    fix(EditTeamButtons.tsx): replace "Delete Team" text with ButtonLoadingSpinner component when loading is true
    
    fix(NoTeamManagement.tsx): replace "Create Team" text with ButtonLoadingSpinner component when loading is true

--- components/custom/SuspenseLoadingSpinner.tsx
+++ components/custom/SuspenseLoadingSpinner.tsx
@@ -1,4 +1,4 @@
-export default function LoadingSpinner() {
+export default function SuspenseLoadingSpinner() {
   return (
     <div className="flex min-h-[200px] items-center justify-center">
       <div className="h-8 w-8 animate-spin rounded-full border-4 border-hackomania-red border-t-transparent"></div>
