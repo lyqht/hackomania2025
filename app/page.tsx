2311d13859368fc3044250a0c683e4868987597e
Sam Tan <54680156+MisterDoobDoob@users.noreply.github.com>
2025-03-14 10:12:26 +0000

    trigger vercel deploy lol

--- app/page.tsx
+++ app/page.tsx
@@ -37,15 +37,6 @@ export default async function Index() {
           LINKTREE
         </span>
       </Link>
-
-      {/* Hide WIP info box */}
-      {/* <div className="w-full max-w-full">
-        <div className="z-30 m-2 rounded-lg border-2 border-hackomania-red p-4 text-lg font-medium text-hackomania-red md:mx-20">
-          <p>
-            We are still actively updating the website! Please check back for more information! :)
-          </p>
-        </div>
-      </div> */}
       <div className="flex flex-1 flex-col overflow-hidden px-5 pt-0 md:px-20 lg:px-32">
         <div className="relative flex min-h-[calc(100vh-120px)] w-full flex-col items-center justify-center md:min-h-[calc(100vh-180px)]">
           <h1 className="sr-only">HackOMania 2025</h1>
